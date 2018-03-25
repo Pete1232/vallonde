a5bc8db729f28c7c494a5bbab9c11a74615e98bc
Pete1232 <pete1232@gmail.com>
2018-03-25 21:05:07 +0100

    Added simple logging

--- src/main/scala/components/upload_serverless_website/handlers/UploadServerlessHandler.scala
+++ src/main/scala/components/upload_serverless_website/handlers/UploadServerlessHandler.scala
@@ -5,15 +5,20 @@ import components.upload_serverless_website.connectors.CodePipelineConnector
 import components.upload_serverless_website.models.SuccessDetails
 import io.circe.HCursor
 import io.circe.parser._
+import org.apache.logging.log4j.{LogManager, Logger}
 
 class UploadServerlessHandler(codePipelineConnector: CodePipelineConnector) extends RequestHandler[String, String] {
   override def handleRequest(input: String, context: Context): String = {
 
+    val logger: Logger = LogManager.getLogger(this.getClass)
+
+    logger.debug("Starting website upload to S3")
     parse(input)
       .flatMap { doc =>
         val cursor: HCursor = doc.hcursor
         cursor.downField("CodePipeline.job").downField("id").as[String]
       }.map { jobId =>
+      logger.debug(s"Called by job $jobId")
       codePipelineConnector.sendSuccessEvent(jobId, SuccessDetails(jobId))
     }.fold(e => s"Error parsing JSON: ${e.getMessage}", _ => input)
   }
