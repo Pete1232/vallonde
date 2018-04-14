04b5c4c790c7dabe7436efef1c64306b8b792c75
Pete1232 <pete1232@gmail.com>
2018-04-14 18:46:14 +0100

    Changed upload serverless to read from an input stream

--- src/main/scala/components/upload_serverless_website/handlers/DefaultUploadServerlessHandler.scala
+++ src/main/scala/components/upload_serverless_website/handlers/DefaultUploadServerlessHandler.scala
@@ -1,8 +1,11 @@
 package components.upload_serverless_website.handlers
 
+import java.io._
+import java.util.stream.Collectors
+
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.amazonaws.services.lambda.runtime.{Context, RequestStreamHandler}
 import components.upload_serverless_website.connectors.DefaultCodePipelineConnector
 import config.global.GlobalConfig
 import connectors.filestore.alpakka.AlpakkaFileStoreConnector
@@ -10,19 +13,27 @@ import connectors.filestore.amazon.{AmazonFileStoreConnector, S3ClientFactory}
 
 import scala.concurrent.ExecutionContext
 
-class DefaultUploadServerlessHandler extends RequestHandler[String, String] {
+class DefaultUploadServerlessHandler extends RequestStreamHandler {
 
   private implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
 
   private implicit val system: ActorSystem = ActorSystem(this.getClass.getSimpleName)
   private implicit val mat: ActorMaterializer = ActorMaterializer()
 
-  override def handleRequest(input: String, context: Context): String = {
-    new UploadServerlessHandler(
+  override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
+
+    val parsedInput: String = new BufferedReader(new InputStreamReader(input))
+      .lines()
+      .collect(Collectors.joining("\n"))
+
+    val result: String = new UploadServerlessHandler(
       new DefaultCodePipelineConnector(),
       new AlpakkaFileStoreConnector(),
       new AmazonFileStoreConnector(S3ClientFactory),
       GlobalConfig
-    ).handleRequest(input, context)
+    ).handleRequest(parsedInput, context)
+
+    new BufferedWriter(new OutputStreamWriter(output))
+      .write(result)
   }
 }
